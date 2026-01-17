f35f4d85e07959c987fc61686f33b2237dce241b
Marggx <maximilian.p.d.marggraf@gmail.com>
2026-01-17 18:13:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# This plugin simply removes water from the dough recipe
+
+![Crafting-Recipe](img/crafting.png)
+
+
+## This plugin was coded live on Twitch join for modding content:
+[![Twitch](https://img.shields.io/badge/Twitch-%239146FF.svg?style=for-the-badge&logo=Twitch&logoColor=white)](https://twitch.tv/marggx)
+
+
+## Found any bugs? Report them here:
+[![Discord](https://img.shields.io/badge/Discord-%235865F2.svg?style=for-the-badge&logo=discord&logoColor=white)](https://discord.gg/Hx5jFT95)
---
+++ src/main/java/dev/marggx/plugin/NoWaterForDough.java
@@ -0,0 +1,26 @@
+package dev.marggx.plugin;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+/**
+ * This class serves as the entrypoint for your plugin. Use the setup method to register into game registries or add
+ * event listeners.
+ */
+public class NoWaterForDough extends JavaPlugin {
+
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public NoWaterForDough(@Nonnull JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+        LOGGER.atInfo().log("Setting up plugin " + this.getName());
+    }
+}
